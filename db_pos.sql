76fcac0cdae9713833765674c335b2eed8526faa
aprilianrizaldy <46649727+aprilianrizaldy@users.noreply.github.com>
2019-01-13 21:31:47 +0700

    Add files via upload

---
+++ db_pos.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 29, 2018 at 02:05 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pembelian`
+--
+
+CREATE TABLE `detail_pembelian` (
+  `kd_pembelian` varchar(100) NOT NULL,
+  `total` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembali` int(11) NOT NULL,
+  `tgl_pembelian` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_pembelian`
+--
+
+INSERT INTO `detail_pembelian` (`kd_pembelian`, `total`, `bayar`, `kembali`, `tgl_pembelian`) VALUES
+('PBL-547544229900634', 132000, 150000, 18000, '26 December 2018');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `kd_penjualan` varchar(100) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembali` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL,
+  `potongan` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_penjualan`
+--
+
+INSERT INTO `detail_penjualan` (`kd_penjualan`, `bayar`, `kembali`, `diskon`, `potongan`, `total`) VALUES
+('PJL-825672595513186', 27000, 1000, 10, 2600, 23400);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `kd_barcode` varchar(25) NOT NULL,
+  `nm_barang` varchar(50) NOT NULL,
+  `satuan` varchar(20) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `profit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`kd_barcode`, `nm_barang`, `satuan`, `harga_beli`, `stok`, `harga_jual`, `profit`) VALUES
+('126267420', 'qwqw', 'PACK', 11000, 31, 13000, 2000),
+('781936684', '123456', 'PCS', 12000, 11, 14000, 2000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `kd_karyawan` varchar(25) NOT NULL,
+  `nama_karyawan` varchar(100) NOT NULL,
+  `tgl_lahir` varchar(50) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `jabatan` enum('Kasir','Admin','Pegawai','Manager','Supervisor') NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`kd_karyawan`, `nama_karyawan`, `tgl_lahir`, `alamat`, `no_telp`, `jk`, `jabatan`, `email`, `username`, `password`, `foto`) VALUES
+('KR-0002', 'Aprilian Rizaldi', '2018-12-26', 'fdff', '1234', 'L', 'Admin', 'sasaasssaasas@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'foto -1545830086.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pembelian`
+--
+
+CREATE TABLE `tb_pembelian` (
+  `id` int(11) NOT NULL,
+  `kd_pembelian` varchar(100) NOT NULL,
+  `nama_suplier` varchar(100) NOT NULL,
+  `kd_barcode` varchar(25) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tanggal_beli` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pembelian`
+--
+
+INSERT INTO `tb_pembelian` (`id`, `kd_pembelian`, `nama_suplier`, `kd_barcode`, `jumlah`, `harga_beli`, `total`, `tanggal_beli`) VALUES
+(1, 'PBL-547544229900634', 'PT Waringin', '126267420', 12, 11000, 132000, '26 December 2018');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penjualan`
+--
+
+CREATE TABLE `tb_penjualan` (
+  `id` int(11) NOT NULL,
+  `kd_penjualan` varchar(100) NOT NULL,
+  `kd_barcode` int(25) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tgl_penjualan` varchar(50) NOT NULL,
+  `nm_pelanggan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_penjualan`
+--
+
+INSERT INTO `tb_penjualan` (`id`, `kd_penjualan`, `kd_barcode`, `jumlah`, `total`, `tgl_penjualan`, `nm_pelanggan`) VALUES
+(2, 'PJL-825672595513186', 126267420, 2, 26000, '2018-12-26', 0);
+
+--
+-- Triggers `tb_penjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `jual` AFTER INSERT ON `tb_penjualan` FOR EACH ROW BEGIN
+UPDATE tb_barang
+SET stok = stok - NEW.jumlah
+WHERE kd_barcode = NEW.kd_barcode;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`kd_barcode`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`kd_karyawan`);
+
+--
+-- Indexes for table `tb_pembelian`
+--
+ALTER TABLE `tb_pembelian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_pembelian`
+--
+ALTER TABLE `tb_pembelian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
